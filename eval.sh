457082dfc52c647370bd568fce4d03466efb3fa1
Ishtiyaque alam <ishtiyaque.alam@hotmail.com>
2026-04-08 19:00:26 +0530

    refresh

--- eval.sh
+++
@@ -1,5 +0,0 @@
-python test.py \
-  --metadata_csv /kaggle/input/datasets/manuelcldg/radiomics-nsclc/phase1_metadata.csv \
-  --clinical_csv /kaggle/input/datasets/saibhossain/clinical-data-of-nsclc-lungi1/NSCLC-Radiomics-Lung1.clinical-version3-Oct-2019.csv \
-  --ckpt ./best.pth \
-  --gtv_margin 10
